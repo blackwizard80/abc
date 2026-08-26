f469405d164892f85c6f28442603ae032d1b319a
blackwizard80 <buvindunisal@gmail.com>
2026-08-26 20:51:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,4 +9,3 @@ SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-�
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
- 
